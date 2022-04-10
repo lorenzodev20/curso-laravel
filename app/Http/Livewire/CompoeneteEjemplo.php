8e2a5d102f2ffe1e6516da073f752921f0c8dfd5
Lorenzo Rojo <lorenzorojo12@gmail.com>
2022-04-09 20:48:28 -0500

    Cambio en el readme

---
+++ app/Http/Livewire/CompoeneteEjemplo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CompoeneteEjemplo extends Component
+{
+    public function render()
+    {
+        return view('livewire.compoenete-ejemplo');
+    }
+}
